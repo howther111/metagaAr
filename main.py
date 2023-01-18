37a5e082d2585fec7be2720ca4ca331b0fa5e7ed
Elauno Mitsugi <atsukiexfeel@yahoo.co.jp>
2023-01-18 11:32:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 import cv2
 from cv2 import aruco
-from arreadtest10 import imgToBoard
+from arreadtest11 import imgToBoard
 import csv
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -17,21 +17,26 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     test = imgToBoard(dict_aruco=aruco.DICT_4X4_50)
     #test.run()
+    ret_porn_dict = {}
     try:
         while True:
-            ret_porn_list = test.output_to_csv()
+            ret_porn_dict = test.output_to_csv(ret_porn_dict=ret_porn_dict)
 
-            if ret_porn_list != None:
+            if ret_porn_dict != {}:
                 with open('porn_list.csv', 'w') as csvfile:
                     writer = csv.writer(csvfile, lineterminator='\n')
 
-                    for porn in ret_porn_list:
-                        writer.writerow([porn.id, porn.x_zahyo, porn.y_zahyo])
+                    for key in ret_porn_dict.keys():
+                        print(key)
+                        print(ret_porn_dict[key].x_zahyo)
+                        print(ret_porn_dict[key].y_zahyo)
+                        writer.writerow([key, ret_porn_dict[key].x_zahyo, ret_porn_dict[key].y_zahyo])
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
     except:
         pass
+        #raise Exception
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
